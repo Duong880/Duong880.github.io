ba104400d620ffd2c98265e51d3c15303891f7d3
Duong880 <duongminhnguyen20@gmail.com>
2021-07-18 13:46:09 -0400

    a

--- web-intensive/lesson1/scriptChat.js
+++ web-intensive/lesson1/scriptChat.js
@@ -1,18 +1,74 @@
-const chk = document.getElementById('chk');
+window.onload = init 
 
-chk.addEventListener('change', () => {
-	document.body.classList.toggle('dark');
-});
+function init() {
+	firebase.auth ().onAuthStateChanged( function (user) {
+		if (user && user.emailVerified){
+			loadConversations(user.email)
+		}
+		else
+		console.log("bbbb");
+	})
+}
 
-// SOCIAL PANEL JS
-const floating_btn = document.querySelector('.floating-btn');
-const close_btn = document.querySelector('.close-btn');
-const social_panel_container = document.querySelector('.social-panel-container');
+let loadConversations = async (email)=>{
+    let currentEmail = email
 
-floating_btn.addEventListener('click', () => {
-	social_panel_container.classList.toggle('visible')
-});
+    let result = await firebase.firestore()
+    .collection('chat')
+    .where('user','array-contains',currentEmail)
+    .get()
 
-close_btn.addEventListener('click', () => {
-	social_panel_container.classList.remove('visible')
-});
+    let Conversations = getDataFromDocs(result.docs)
+    console.log(Conversations);
+}
+
+let signOut = ()=>{
+	firebase.auth().signOut().then( function() {
+		window.open('./signIn.html','_self')
+	}, function(error) {
+		alert('Sign Out Error', error)
+	})
+}
+
+let getDataFromDoc = (doc)=>{
+    let data = doc.data()
+    data.id = doc.id
+    return data
+
+}
+
+let getDataFromDocs = (docs)=>{
+   let result =[]
+   for(let doc of docs){
+       let data = getDataFromDoc(doc)
+       result.push(data)
+    }
+   return result
+}
+
+
+let clockChat = ()=>{
+
+    let date = new Date()
+    let h = date.getHours()
+    let m = date.getMinutes()
+    let s = date.getSeconds()
+    let d = date.getDay()
+
+    if(m < 10){
+        m = "0" + m
+    }
+    if(h < 10){
+        h = "0" + h
+    }
+    if(s < 10){
+        s = "0" + s
+    }
+  var weekday = new Array(7);
+  weekday[0] = "Sunday";
+  weekday[1] = "Monday";
+  weekday[2] = "Tuesday";
+  weekday[3] = "Wednesday";
+  weekday[4] = "Thursday";
+  weekday[5] = "Friday";
+  weekday[6] = "Saturday
